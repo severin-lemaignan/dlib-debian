5b28129a7f1e2ffef736193517c6977614a1ad17
Séverin Lemaignan <severin.lemaignan@plymouth.ac.uk>
2016-02-08 12:25:15 +0000

    Remove external dep src from upstream tarball

--- debian/orig-tar.sh
+++ debian/orig-tar.sh
@@ -1,16 +1,16 @@
 #/bin/sh -e
 
 VERSION=$2
-TAR=../dlib_$VERSION.orig-models.tar.gz
-NEWTAR=../dlib_$VERSION.orig-dlib-models.tar.bz2
-DIR=dlib-models
+TAR=../dlib_$VERSION.orig.tar.gz
+NEWTAR=../dlib_$VERSION.orig.tar.bz2
+DIR=dlib-$VERSION
 mkdir -p $DIR
 
 # Unpack ready fo re-packing
 tar -xzf $TAR -C $DIR --strip-components=1
 
-# Uncompress the models
-bzip2 -d $DIR/*.bz2
+# Remove included sources for external libraries (libjpeg, libpng, zlib)
+rm -Rf $DIR/dlib/external
 
 # Repack
 rm $TAR
